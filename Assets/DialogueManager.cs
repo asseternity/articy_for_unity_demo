d9b1c039cbbea5c5a56541414c130e18e38e14a7
asseternity <nakupovasset@gmail.com>
2025-02-22 20:54:45 +0400

    feat: init branching paths buttons spawns

--- Assets/DialogueManager.cs
+++ Assets/DialogueManager.cs
@@ -22,10 +22,10 @@ public class DialogueManager : MonoBehaviour, IArticyFlowPlayerCallbacks
     Text dialogueSpeaker;
 
     [SerializeField]
-    Button dialogueButton;
+    RectTransform branchLayoutPanel;
 
     [SerializeField]
-    Button endDialogueButton;
+    GameObject branchPrefab;
 
     // To check if we are currently showing the dialog ui interface
     public bool DialogueActive { get; set; }
@@ -35,8 +35,6 @@ public class DialogueManager : MonoBehaviour, IArticyFlowPlayerCallbacks
     void Start()
     {
         flowPlayer = GetComponent<ArticyFlowPlayer>();
-        dialogueButton.onClick.AddListener(ContinueDialogue);
-        endDialogueButton.onClick.AddListener(CloseDialogueBox);
     }
 
     public void StartDialogue(IArticyObject aObject)
@@ -44,21 +42,12 @@ public class DialogueManager : MonoBehaviour, IArticyFlowPlayerCallbacks
         DialogueActive = true;
         dialogueWidget.SetActive(DialogueActive);
         flowPlayer.StartOn = aObject;
-        // Set the dialogue button active again (we hide it when we reach the end in OnBranchesUpdated)
-        dialogueButton.gameObject.SetActive(DialogueActive);
-    }
-
-    public void ContinueDialogue()
-    {
-        flowPlayer.Play();
     }
 
     public void CloseDialogueBox()
     {
         DialogueActive = false;
         dialogueWidget.SetActive(DialogueActive);
-        // When we hide the dialogue UI, we also want to set the Close button back to an inactive state
-        endDialogueButton.gameObject.SetActive(DialogueActive);
         // Last object might have an output pin containing scripts, so we execute them by telling flowPlayer to finish current paused object
         flowPlayer.FinishCurrentPausedObject();
     }
@@ -69,16 +58,20 @@ public class DialogueManager : MonoBehaviour, IArticyFlowPlayerCallbacks
         //Clear data
         dialogueText.text = string.Empty;
         dialogueSpeaker.text = string.Empty;
+
         // If we paused on an object that has a "Text" property fetch this text and present it
         var objectWithText = aObject as IObjectWithLocalizableText;
         if (objectWithText != null)
         {
             dialogueText.text = objectWithText.Text;
         }
+
         // If we paused on an object with a speaker name
         var objectWithSpeaker = aObject as IObjectWithSpeaker;
         if (objectWithSpeaker != null)
         {
+            // If the object has a "Speaker" property, fetch the reference
+            // and ensure it is really set to an "Entity" object to get its "DisplayName"
             var speakerEntity = objectWithSpeaker.Speaker as Entity;
             if (speakerEntity != null)
             {
@@ -87,22 +80,38 @@ public class DialogueManager : MonoBehaviour, IArticyFlowPlayerCallbacks
         }
     }
 
+    // Called every time the flow player encounters multiple branches,
+    // or is paused on a node and wants to tell us how to continue
     public void OnBranchesUpdated(IList<Branch> aBranches)
     {
+        // Destroy buttons from previous use, will create new ones here
+        ClearAllBranches();
+
         // Here we get passed a list of all branches following the current node
+        // So we check if any branch leads to a DialogueFragment target
+        // If so, the dialogue is not yet finished
         bool dialogIsFinished = true;
         foreach (var branch in aBranches)
         {
-            // As long as we have Dialogue Fragment following the current node, the dialogue continues
             if (branch.Target is IDialogueFragment)
             {
                 dialogIsFinished = false;
             }
         }
-        if (dialogIsFinished)
+        if (!dialogIsFinished)
         {
-            dialogueButton.gameObject.SetActive(false);
-            endDialogueButton.gameObject.SetActive(true);
+            foreach (var branch in aBranches)
+            {
+                GameObject btn = Instantiate(branchPrefab, branchLayoutPanel);
+            }
+        }
+    }
+
+    private void ClearAllBranches()
+    {
+        foreach (Transform child in branchLayoutPanel)
+        {
+            Destroy(child.gameObject);
         }
     }
 }
